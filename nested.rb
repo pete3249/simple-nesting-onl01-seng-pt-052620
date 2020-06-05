ad1a2ed942d42bf857cb5d523ab7024942100888
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-05 03:49:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     binding.pry
-  programmer_hash[:dennis_ritchie][:languages][2] = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
   programmer_hash
 end
