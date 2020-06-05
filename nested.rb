db0c01621bc410c1e5675fd2b26a8a4802fbc612
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-05 03:47:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,17 +77,6 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto] = {
       :known_for => "Ruby",
       :languages => ["LISP", "C"]}
-  
-  
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-   
-     
-     
-     
   programmer_hash
 end
 
@@ -107,10 +96,12 @@ def changing_alan
           :languages => ["C"]
         }
      }
+  
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+  programmer_hash
 end
 
 def adding_to_dennis
